489fee911ee3f84b1da7fb061fd904aded8b3cd8
Oskarmay <oamajewski@gmail.com>
2023-09-22 20:39:57 +0200

    add comments to main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -13,16 +13,25 @@ import 'primevue/resources/themes/bootstrap4-dark-purple/theme.css'
 import './assets/main.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+
+// App instance
 const app = createApp(App)
 
+// Axios configuration
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://localhost:5000'
 
+// Initialize pinia
 app.use(createPinia())
+
+// Initialize vue router
 app.use(router)
+
+// Initialize prime-vue
 app.use(PrimeVue)
 
 //Prime-vue components
 app.component('PButton', Button)
 
+// app mounting
 app.mount('#app')
